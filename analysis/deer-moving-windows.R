4f52c1fa9ca6995cc922722ed33b177f9fde3d67
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-03-21 15:25:01 -0700

    code to fit run the moving windows in parallel

---
+++ analysis/deer-moving-windows.R
@@ -0,0 +1,37 @@
+library('tidyr') #' for `nest()`
+library('furrr') # for fast parallel computations
+source('functions/window_ctmm.R')
+
+# model for the estimated root-mean-square User Equivalent Range Error
+deer_uere <- readRDS('models/calibration.error.model.inventa_no_class.rds')
+
+# import both years of data
+d <- bind_rows(read.csv('data/Year_1_processed_data.csv'),
+               read.csv('data/Year_2_processed_data.csv')) %>%
+  mutate(animal = individual.local.identifier) %>%
+  group_by(study.year, animal) %>% # animals were collared each year
+  nest(tel = - c(study.year, animal)) %>%
+  mutate(tel = map(tel, \(.tel) {
+    .tel <- as.telemetry(.tel)
+    uere(.tel) <- deer_uere # add collar error calibration
+    return(.tel)
+  }))
+
+# ensure years 1 and 2 are labelled separately
+unique(d$study.year)
+
+# set number of cores to run on -- will be parallelizing by collar
+#' **do not set CORES at the number of cores you have available or higher**
+CORES <- availableCores(logical = FALSE) - 2
+
+plan(multisession, workers = CORES)
+
+map(d$tel, function(.d) {
+  cat('Running', .d@info$identity, '\n')
+  .d <- window_hr(.d,
+                  window = 14 %#% 'day',
+                  dt = 14 %#% 'day',
+                  fig_path = 'figures/moving-window',
+                  rds_path = 'models/moving-window',
+                  cores = 1) # cannot parallelize on Windows
+})
