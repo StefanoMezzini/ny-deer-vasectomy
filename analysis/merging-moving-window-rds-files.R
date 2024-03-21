caa3d24c11efcb526a0a6516129756cb29d190e3
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-03-21 13:35:31 -0700

    make OU speeds NA, add tels, ctmms and DOF

--- analysis/merging-moving-window-rds-files.R
+++ analysis/merging-moving-window-rds-files.R
@@ -41,11 +41,24 @@ unique(d$hr_units)
 unique(d$speed_model_units) # from the speed() function
 unique(d$speed_gauss_units) # from the movement model
 
+# change missing speeds to NA
+d <- mutate(d,
+            speed_gauss_lwr = if_else(is.finite(speed_gauss_est), speed_gauss_lwr, NA_real_),
+            speed_gauss_est = if_else(is.finite(speed_gauss_est), speed_gauss_est, NA_real_),
+            speed_gauss_upr = if_else(is.finite(speed_gauss_est), speed_gauss_upr, NA_real_),
+            speed_model_lwr = if_else(is.finite(speed_gauss_est), speed_model_lwr, NA_real_),
+            speed_model_est = if_else(is.finite(speed_gauss_est), speed_model_est, NA_real_),
+            speed_model_upr = if_else(is.finite(speed_gauss_est), speed_model_upr, NA_real_))
+
 # only keep necessary columns
 d <- d %>%
+  rename(tel = dataset) %>%
   select(
     study_year,
     animal,
+    tel,
+    model,
+    akde,
     tau_p_units, tau_p_est, tau_p_lwr, tau_p_upr,
     tau_v_units, tau_v_est, tau_v_lwr, tau_v_upr,
     speed_gauss_units, speed_gauss_est, speed_gauss_lwr, speed_gauss_upr,
@@ -59,7 +72,16 @@ d <- d %>%
 metadata <- read.csv('data/reference_data.csv') %>%
   rename(animal = id)
 
-d_final <- left_join(d, metadata, by = c('animal', 'study_year'))
+d_final <- left_join(d, metadata, by = c('animal', 'study_year')) %>%
+  mutate(animal = factor(animal),
+         study_year = factor(study_year),
+         animal_year = factor(paste(animal, study_year)),
+         sex_treatment = factor(paste(sex, study_site)),
+         s_t_y = factor(paste(sex, study_site, study_year)),
+         # dof_area from ctmm and akde are identical
+         dof_area = map_dbl(model, \(.m) summary(.m)$DOF['area']),
+         dof_diff = map_dbl(model, \(.m) summary(.m)$DOF['diffusion']),
+         dof_speed = map_dbl(model, \(.m) summary(.m)$DOF['speed']))
 
 # save the final dataset
 saveRDS(object = d_final, file = 'data/years-1-and-2-data.rds')
