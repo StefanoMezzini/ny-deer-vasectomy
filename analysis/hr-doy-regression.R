d3b919974469dbbfb9b0503923d0bc80e12c21ec
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-03-23 12:37:08 -0700

    fit model with HRs < 7.5 only

--- analysis/hr-doy-regression.R
+++ analysis/hr-doy-regression.R
@@ -1,11 +1,13 @@
 library('dplyr')     # for data wrangling
 library('tidyr')     # for data wrangling
+library('purrr')     # for functional programming
 library('mgcv')      # for modeling
 library('lubridate') # for working with dates
 library('ggplot2')    # for fancy plots
 library('gratia')    # for predicting from models
 theme_set(theme_bw() + theme(text = element_text(face = 'bold')))
-source('functions/gammals-variance-simulation-and-derivatives.R')
+source('functions/gammals-variance-simulation-cis.R')
+source('analysis/ref_dates.R')
 
 d <- readRDS('data/years-1-and-2-data.rds') %>%
   mutate(animal = factor(animal),
@@ -14,7 +16,11 @@ d <- readRDS('data/years-1-and-2-data.rds') %>%
          s_t_y = factor(paste(sex, study_site, study_year))) %>%
   # drop first 10 days for each animal to remove odd behaviors
   group_by(animal, study_year) %>%
-  filter(date >= min(date) + 10)
+  filter(date >= min(date) + 10) %>%
+  ungroup() %>%
+  mutate(dof_area = map_dbl(model, \(.m) summary(.m)$DOF['area']))
+
+quantile(d$dof_area, probs = c(0, 0.1, 0.2, 0.25))
 
 # data is not continuous throughout the year
 hist(yday(d$date))
@@ -28,37 +34,35 @@ d <- mutate(d,
               date - as.Date(paste0(year(date), '-08-01')),
               # otherwise calculate days since Aug 1 of previous year 
               date - as.Date(paste0(year(date) - 1, '-08-01'))) %>%
-              as.numeric(), # convert difftime to numeric
-            weight = 1 / (hr_upr_95 - hr_lwr_95)) %>% # account for HR uncertainty
-  group_by(animal) %>%
-  mutate(weight = weight / mean(weight)) %>% # normalize weights
-  ungroup()
-
-hist(d$days_since_aug_1) # now without breaks
-
-# ensure weights are correct
-d %>%
-  group_by(animal) %>%
-  summarize(weights = round(sum(weight)),
-            count = n()) %>%
-  mutate(check = weights == count) %>%
-  pull(check) %>%
-  all()
+              as.numeric()) # convert difftime to numeric
 
 # plot the data ----
 # plot overall raw data
 ggplot(d, aes(days_since_aug_1, hr_est_95, group = animal)) +
-  facet_wrap(~ sex_treatment) +
+  facet_wrap(~ sex_treatment + study_year, ncol = 2) +
   # coord_cartesian(ylim = c(0, 15)) +
   geom_line()
 
 # using free scales to view cycles more clearly
-ggplot(d, aes(days_since_aug_1, hr_est_95)) +
-  facet_wrap(~ sex_treatment, scales = 'free_y') +
-  geom_vline(xintercept = 100, col = 'red') +
-  geom_line(aes(group = animal))
+p_hr <-
+  mutate(d,
+         sex = if_else(sex == 'f', 'females', 'males'),
+         treatment = if_else(study_site == 'rockefeller', 'Rockefeller',
+                             'Staten Island'),
+         t_s = paste(treatment, sex),
+         study_year = paste('Year', study_year)) %>%
+  ggplot(aes(date, hr_est_95)) +
+  facet_grid(t_s ~ study_year, scales = 'free') +
+  geom_vline(xintercept = REF_DATES, col = 'red') +
+  geom_line(aes(group = animal)) +
+  labs(x = NULL,
+       y = expression(bold('Estimated 7-day space-use requirements'~(km^2))))
+p_hr
 
-# find odd male from S Island
+ggsave('figures/hr-estimates.png', p_hr, width = 8, height = 8, dpi = 600,
+       bg = 'white')
+
+# find males with large HRs on S Island
 filter(d, sex_treatment == 'm staten_island', hr_est_95 > 15) %>%
   select(study_year, animal, Age)
 
@@ -66,68 +70,90 @@ filter(d, sex_treatment == 'm staten_island', hr_est_95 > 15) %>%
 filter(d, sex_treatment == 'm rockefeller', hr_est_95 > 7.5) %>%
   select(study_year, animal, Age)
 
-# start by fitting a Hierarchical Generalized Additive Model ----
-# not using cyclic cubic splines because the gap is too big (66 days)
+# fit a Hierarchical Generalized Additive Model for Location and Scale ----
+# not using cyclic cubic splines because the gap is too big
 # for year 1 the gap is even bigger
 range(d$days_since_aug_1) # not close to 0 to 365
-365 - (317 - 18)
+365 - diff(range(d$days_since_aug_1))
+
+# some very high HRs
+quantile(d$hr_est_95, c(0.95, 0.97, 0.98, 0.99, 1))
+# dropping HRs > 7.5 barely drops < 2% of the data
+round(1 - ecdf(d$hr_est_95)(7.5), 3)
 
 # location-scale model ----
 if(FALSE) {
   m_hr <- gam(formula = list(
-    #' not using fixed effects of `sex_treatment` because they are too
-    #' uncertain due to the large HRs in May.
-    #' the REs of `sex_treatment` are much more constrained.
-    
     # linear predictor for the mean
     hr_est_95 ~
-      # random intercept of sex and treatment
-      s(sex_treatment, bs = 're') +
       # temporal sex- and treatment-level trends with different
-      s(days_since_aug_1, sex_treatment, k = 10, bs = 'fs') +
+      s(days_since_aug_1, by = sex_treatment, k = 10) +
       # accounts for differences in trends between years
-      s(days_since_aug_1, s_t_y, k = 10, bs = 'fs') +
+      s(days_since_aug_1, by = sex_treatment, study_year, k = 10, bs = 'sz') +
       # accounts for differences between individuals
       s(animal, bs = 're'),
     
     # linear predictor for the scale (sigma2 = mu^2 * scale)
     # allows mean-variance relationship to be different between sexes
     # sex- and treatment-level trends over season
-    ~ s(days_since_aug_1, sex_treatment, k = 5, bs = 'fs')),
+    ~ s(days_since_aug_1, sex_treatment, k = 6, bs = 'fs')),
     
     family = gammals(),
     data = d,
-    weights = weight,
+    subset = hr_est_95 < 7.5, #' **drop extreme HR estimates**
+    weights = dof_area,
     method = 'REML',
     control = gam.control(trace = TRUE))
   
-  saveRDS(m_hr, paste0('models/m_hr-hgamls-', Sys.Date(), '.rds'))
-} else {
-  m_hr <- readRDS('models/m_hr-hgamls-2024-03-15.rds')
-}
-
-draw(m_hr, scales = 'fixed')
-
-# check the model diagnostics ----
-if(FALSE) {
+  beepr::beep()
   appraise(m_hr, point_alpha = 0.1) # overdispersed residuals
   
-  # check trends in the residuals
-  d$e <- residuals(m_hr)
-  
-  # no visible trends in the residuals over day of year
-  ggplot(d, aes(days_since_aug_1, e)) +
-    facet_grid(sex ~ study_site) +
-    geom_point(alpha = 0.1) +
-    geom_smooth()
-  
-  # check residuals across groups
-  ggplot(d, aes(e)) +
-    facet_grid(sex ~ study_site) +
-    geom_vline(xintercept = 0) +
-    geom_density(fill = 'grey', bw = 0.25) +
-    scale_y_continuous(expand = c(0, 0), limits = c(0, 0.63))
+  saveRDS(m_hr, paste0('models/m_hr-hgamls-', Sys.Date(), '.rds'))
+} else {
+  m_hr <- readRDS('models/m_hr-hgamls-2024-03-23.rds')
 }
+
+plot(m_hr, pages = 1)
+
+ggplot() +
+  geom_point(aes(m_hr$fitted.values[, 1], m_hr$model$hr_est_95),
+             alpha = 0.2) +
+  geom_abline(intercept = 0, slope = 1, color = 'red')
+
+# check what groups cause oddly large outliers
+d %>%
+  filter(hr_est_95 < 7.5) %>%
+  filter(hr_est_95 > 4, m_hr$fitted.values[, 1] < 4) %>%
+  group_by(sex_treatment) %>%
+  summarise(n = n(), .groups = 'drop') %>%
+  mutate(prop = round(n / sum(n), 2))
+
+d %>%
+  filter(hr_est_95 < 7.5) %>%
+  mutate(sex = if_else(sex == 'f', 'Females', 'Males'),
+       treatment = if_else(study_site == 'rockefeller', 'Rockefeller',
+                           'Staten Island'),
+       fitted = m_hr$fitted.values[, 1],
+       large = resid(m_hr) > 3) %>%
+  ggplot() +
+  facet_grid(treatment ~ sex) +
+  geom_point(aes(fitted, hr_est_95, color = large, alpha = large)) +
+  labs(x = 'Fitted values', y = 'Observed values') +
+  scale_color_manual('Deviance residuals > 3', values = 1:2) +
+  scale_alpha_manual('Deviance residuals > 3', values = c(0.3, 1)) +
+  theme(legend.position = 'top')
+
+ggsave('figures/hr-model-obs-fitted.png', width = 8, height = 8, dpi = 600,
+       bg = 'white')
+
+# check periods of oddly large outliers
+d %>%
+  filter(hr_est_95 < 7.5) %>%
+  filter(hr_est_95 > 4, m_hr$fitted.values[, 1] < 4) %>%
+  ggplot() +
+  facet_grid(study_site ~ sex) +
+  geom_histogram(aes(days_since_aug_1), color = 'black', fill = 'grey',
+                 bins = 6)
 
 # plot the estimated trends common between the two years ----
 newd <-
@@ -153,62 +179,71 @@ newd <-
 preds_mu <- gammals_mean(model = m_hr, data = newd, nsims = 1e4,
                          unconditional = FALSE,
                          # not excluding the term results in NA
-                         exclude = 's(days_since_aug_1,s_t_y)') %>%
+                         exclude = c('s(days_since_aug_1,study_year):sex_treatmentf rockefeller',
+                                     's(days_since_aug_1,study_year):sex_treatmentf staten_island',
+                                     's(days_since_aug_1,study_year):sex_treatmentm rockefeller',
+                                     's(days_since_aug_1,study_year):sex_treatmentm staten_island')) %>%
   group_by(date, sex_treatment, days_since_aug_1, sex, site) %>%
   summarize(lwr_95 = quantile(mean, 0.025),
             mu = quantile(mean, 0.500),
             upr_95 = quantile(mean, 0.975),
             .groups = 'drop') %>%
   mutate(sex = case_when(sex == 'f' ~ 'Female',
-                             sex == 'm' ~ 'Male'),
+                         sex == 'm' ~ 'Male'),
          site = case_when(site == 'rockefeller' ~ 'Rockefeller',
-                              site == 'staten_island' ~ 'Staten Island'))
+                          site == 'staten_island' ~ 'Staten Island'))
 
-preds_s2 <- gammals_var(model = m_hr, data = newd, nsims = 1e4,
+preds_s <- gammals_var(model = m_hr, data = newd, nsims = 1e4,
                         unconditional = FALSE,
                         # not excluding the term results in NA
-                        exclude = 's(days_since_aug_1,s_t_y)') %>%
+                        exclude = c('s(days_since_aug_1,study_year):sex_treatmentf rockefeller',
+                                    's(days_since_aug_1,study_year):sex_treatmentf staten_island',
+                                    's(days_since_aug_1,study_year):sex_treatmentm rockefeller',
+                                    's(days_since_aug_1,study_year):sex_treatmentm staten_island')) %>%
   group_by(date, sex_treatment, days_since_aug_1, sex, site) %>%
   summarize(lwr_95 = quantile(variance, 0.025),
             s2 = quantile(variance, 0.500),
             upr_95 = quantile(variance, 0.975),
             .groups = 'drop') %>%
   mutate(sex = case_when(sex == 'f' ~ 'Female',
-                             sex == 'm' ~ 'Male'),
+                         sex == 'm' ~ 'Male'),
          site = case_when(site == 'rockefeller' ~ 'Rockefeller',
-                              site == 'staten_island' ~ 'Staten Island'))
+                          site == 'staten_island' ~ 'Staten Island'))
 
 # mean HR
+DATES <- as.Date(c('2021-09-15', '2021-11-15', '2022-01-15', '2022-03-15',
+                   '2022-05-15'))
+LABS <- format(DATES, '%B 15')
 p_mu <-
   ggplot(preds_mu, aes(group = sex_treatment)) +
   facet_grid(sex ~ .) +
-  geom_vline(xintercept = as.Date('2021-11-09'), col = 'red')+
+  geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
   geom_ribbon(aes(date, ymin = lwr_95, ymax = upr_95, fill = site),
-              alpha = 0.2) +
-  geom_line(aes(date, mu, color = site)) +
-  scale_color_brewer('Site', type = 'qual', palette = 2,
+              alpha = 0.3) +
+  geom_line(aes(date, mu, color = site), lwd = 1) +
+  scale_color_brewer('Site', type = 'qual', palette = 1,
                      aesthetics = c('color', 'fill')) +
   scale_linetype('Site') +
-  labs(x = NULL,
-       y = expression(bold('Mean 7-day home range size'~(km^2)))) +
+  scale_x_continuous(NULL, breaks = DATES, labels = LABS) +
+  ylab(expression(bold('Mean 7-day space-use requirements'~(km^2)))) +
   theme(legend.position = 'top'); p_mu
 
 ggsave('figures/hr-mean.png', p_mu, width = 8, height = 8, dpi = 600,
        bg = 'white')
 
-# variance in HR ---
+# standard deviation in HR ---
 p_s <-
-  ggplot(preds_s2) +
+  ggplot(preds_s) +
   facet_grid(sex ~ .) +
-  geom_vline(xintercept = as.Date('2021-11-09'), col = 'red')+
+  geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
   geom_ribbon(aes(date, ymin = sqrt(lwr_95), ymax = sqrt(upr_95),
                   fill = site), alpha = 0.3) +
-  geom_line(aes(date, sqrt(s2), color = site)) +
-  scale_color_brewer('Site', type = 'qual', palette = 2,
+  geom_line(aes(date, sqrt(s2), color = site), lwd = 1) +
+  scale_color_brewer('Site', type = 'qual', palette = 1,
                      aesthetics = c('color', 'fill')) +
   scale_linetype('Site') +
-  labs(x = NULL,
-       y = expression(bold('SD in 7-day home range size'~(km^2)))) +
+  scale_x_continuous(NULL, breaks = DATES, labels = LABS) +
+  ylab(expression(bold('SD in 7-day space-use requirements'~(km^2)))) +
   theme(legend.position = 'top'); p_s
 
 ggsave('figures/hr-sd.png', p_s, width = 8, height = 8, dpi = 600,
@@ -245,10 +280,10 @@ preds_mu_years <-
             .groups = 'drop') %>%
   mutate(sex = case_when(sex == 'f' ~ 'Female',
                          sex == 'm' ~ 'Male'),
-         site_ = case_when(site == 'rockefeller' ~ 'Rockefeller',
+         site = case_when(site == 'rockefeller' ~ 'Rockefeller',
                           site == 'staten_island' ~ 'Staten Island'))
 
-preds_s2_years <-
+preds_s_years <-
   gammals_var(model = m_hr, data = newd_years, nsims = 1e4,
               unconditional = FALSE) %>%
   group_by(date, sex_treatment, days_since_aug_1, sex, site, study_year)%>%
@@ -262,37 +297,38 @@ preds_s2_years <-
                           site == 'staten_island' ~ 'Staten Island'))
 
 # mean HR
-p_mu <-
+p_mu_y <-
   ggplot(preds_mu_years) +
-  facet_grid(sex ~ study_year) +
-  geom_vline(xintercept = as.Date('2021-11-09'), col = 'red')+
+  facet_grid(sex ~ paste('Year', study_year)) +
+  geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
   geom_ribbon(aes(date, ymin = lwr_95, ymax = upr_95, fill = site),
-              alpha = 0.2) +
-  geom_line(aes(date, mu, color = site)) +
-  scale_color_brewer('Site', type = 'qual', palette = 2,
+              alpha = 0.3) +
+  geom_line(aes(date, mu, color = site), lwd = 1) +
+  scale_color_brewer('Site', type = 'qual', palette = 1,
                      aesthetics = c('color', 'fill')) +
   scale_linetype('Site') +
-  labs(x = NULL,
-       y = expression(bold('Mean 7-day home range size'~(km^2)))) +
-  theme(legend.position = 'top'); p_mu
+  scale_x_continuous(NULL, breaks = DATES, labels = LABS) +
+  ylab(expression(bold('Mean 7-day space-use requirements'~(km^2)))) +
+  theme(legend.position = 'top'); p_mu_y
 
-ggsave('figures/hr-mean-years.png', p_mu, width = 8, height = 8, dpi = 600,
-       bg = 'white')
+ggsave('figures/hr-mean-years.png',
+       p_mu_y, width = 8, height = 8, dpi = 600, bg = 'white')
 
 # variance in HR ---
 p_s <-
-  ggplot(preds_s2_years, aes(group = sex_treatment)) +
-  facet_grid(sex ~ study_year) +
-  geom_vline(xintercept = as.Date('2021-11-09'), col = 'red')+
+  ggplot(preds_s_years, aes(group = sex_treatment)) +
+  facet_grid(sex ~ paste('Year', study_year)) +
+  geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
   geom_ribbon(aes(date, ymin = sqrt(lwr_95), ymax = sqrt(upr_95),
                   fill = site), alpha = 0.3) +
-  geom_line(aes(date, sqrt(s2), color = site)) +
-  scale_color_brewer('Site', type = 'qual', palette = 2,
+  geom_line(aes(date, sqrt(s2), color = site), lwd = 1) +
+  scale_color_brewer('Site', type = 'qual', palette = 1,
                      aesthetics = c('color', 'fill')) +
   scale_linetype('Site') +
-  labs(x = NULL,
-       y = expression(bold('SD in 7-day home range size'~(km^2)))) +
+  scale_x_continuous(NULL, breaks = DATES, labels = LABS) +
+  ylab(expression(bold('SD in 7-day space-use requirements'~(km^2)))) +
   theme(legend.position = 'top'); p_s
 
 ggsave('figures/hr-sd-years.png', p_s, width = 8, height = 8, dpi = 600,
        bg = 'white')
+  
---
+++ analysis/ref_dates.R
@@ -0,0 +1,2 @@
+REF_DATES <- as.Date(c(paste0(2021:2022, '-11-09'),  # start
+                       paste0(2022:2023, '-04-01'))) # end
