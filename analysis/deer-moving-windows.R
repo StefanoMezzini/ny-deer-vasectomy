5049b7664acbce36d3d1c40b7454dff63d566ba5
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-04-19 15:11:01 -0700

    split moving windows by study year

--- analysis/deer-moving-windows.R
+++ analysis/deer-moving-windows.R
@@ -1,3 +1,7 @@
+###########################################################################
+#' **not run; the moving widows were fit by Petar.**
+#' the code should produce something equivalent
+###########################################################################
 library('tidyr') #' for `nest()`
 library('furrr') # for fast parallel computations
 source('functions/window_ctmm.R')
@@ -8,9 +12,10 @@ deer_uere <- readRDS('models/calibration.error.model.inventa_no_class.rds')
 # import both years of data
 d <- bind_rows(read.csv('data/Year_1_processed_data.csv'),
                read.csv('data/Year_2_processed_data.csv')) %>%
-  mutate(animal = individual.local.identifier) %>%
+  mutate(animal = individual.local.identifier,
+         collar = paste0(animal, '_y', study.year)) %>%
   group_by(study.year, animal) %>% # animals were collared each year
-  nest(tel = - c(study.year, animal)) %>%
+  nest(tel = - c(study.year, animal, collar)) %>%
   mutate(tel = map(tel, \(.tel) {
     .tel <- as.telemetry(.tel)
     uere(.tel) <- deer_uere # add collar error calibration
@@ -23,7 +28,7 @@ unique(d$study.year)
 
 # for a quick test
 if(FALSE) {
-  test <- window_ctmm(d$tel[[1]][c(1:10, 100:110), ],
+  test <- window_ctmm(d$tel[[1]][c(1:6, 120:145), ],
                       window = 1 %#% 'day',
                       dt = 1 %#% 'day',
                       fig_path = NULL,
@@ -33,8 +38,8 @@ if(FALSE) {
 }
 
 # set number of cores to run on -- will be parallelizing by collar
-#' **do not set CORES at the number of cores you have available or higher**
-CORES <- availableCores(logical = FALSE) - 2
+#' **do not set CORES at or above the number of cores you have available**
+CORES <- availableCores(logical = FALSE) - 1
 plan(multisession, workers = CORES)
 
 future_map2(d$tel, d$study.year, function(.telemetry, .sy) {
@@ -42,8 +47,8 @@ future_map2(d$tel, d$study.year, function(.telemetry, .sy) {
               study_year = .sy,
               window = 14 %#% 'day',
               dt = 14 %#% 'day',
-              fig_path = 'figures/moving-window',
-              rds_path = 'models/moving-window',
+              fig_path = paste0('figures/moving-window/', d$study.year),
+              rds_path = paste0('models/moving-window/', d$study.year),
               cores = 1, # cannot parallelize on Windows
               progress = 0) # can't have progress if parallelized
 },
