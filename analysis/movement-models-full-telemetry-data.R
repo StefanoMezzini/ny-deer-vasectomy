5a1a68105e585d3b2889d18565a921782eb3e2d6
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-04-19 15:16:04 -0700

    updated script to find density for each location

--- analysis/movement-models-full-telemetry-data.R
+++ analysis/movement-models-full-telemetry-data.R
@@ -4,6 +4,11 @@ library('tidyr') # for data wrangling
 library('purrr') # for functional programming
 library('furrr') # for parallel computing
 
+extract_hr <- function(.ud, par, l.ud) {
+  summary(.ud, units = FALSE, level = 0.95,
+          level.UD = l.ud)$CI['area (square meters)', par]
+}
+
 # set up parallel computation plan (separate R sessions)
 CORES <- availableCores(logical = FALSE) - 1
 plan(multisession, workers = CORES)
@@ -35,55 +40,46 @@ d <-
         # estimate autocorrelated kernel density estimate
         akde = akde(data = .tel, CTMM = model[[1]],
                     weights = TRUE) %>% # weights using sampling frequency
-          list())
-    },
-    .progress = TRUE)) %>%
-  unnest(models)
+          list(),
+        # calculate estimated average density at each location 
+        tel = map2(tel, akde, \(.tel, .akde) {
+          .r <- raster(.akde, DF = 'PDF')
+          .tel$density <- raster::extract(.r, SpatialPoints.telemetry(.tel))
+        }),
+        # find home range estimate
+        hr_est_95 = extract_hr(.ud = akde[[1]], par='est', l.ud=0.95),
+        # # find degrees of freedom
+        dof_area = ctmm:::DOF.area(model[[1]]),
+        dof_diff = summary(model[[1]])$DOF['diffusion'],
+        dof_speed = ctmm:::DOF.speed(model[[1]]))
+    }, .progress = TRUE, .options = furrr_options(seed = TRUE))) %>%
+  tidyr::unnest(models) %>%
+  mutate(mean_diffusion = map_dbl(model, \(.m) { # find diffusion estimates
+    if(any(grepl('diffusion',
+                 rownames(summary(.m)$CI)))) {
+      return(summary(.m, units = FALSE)$
+               CI['diffusion (square meters/second)', 'est'])
+    } else {
+      return(NA_real_)
+    }}),
+    # find speed
+    mean_speed = map_dbl(model, \(.m) {
+      if(any(grepl('speed',
+                   rownames(summary(.m)$CI)))) {
+        return(summary(.m, units = FALSE)$
+                 CI['speed (meters/second)', 'est'])
+      } else {
+        return(NA_real_)
+      } #' close `else`
+    } #' close `function`
+    ),
+    #' #' `ctmm::%#%` syntax: `"new units" %#% value in SI units`
+    units_area = 'km^2',
+    hr_est_95 = units_area %#% hr_est_95,
+    units_diff = 'km^2/day',
+    mean_diffusion = units_diff %#% mean_diffusion,
+    units_speed = 'km/day',
+    mean_speed = units_speed %#% mean_speed)
 
-# extract movement parameters ----
-#' # find home range estimate
-#' summary(full_ud, units = FALSE)$CI['area (square meters)', 'est']
-#' hr_est_95 = extract_hr(a = akde[[1]], par='est', l.ud=0.95),
-#' hr_lwr_95 = extract_hr(a = akde[[1]], par='low', l.ud=0.95),
-#' hr_upr_95 = extract_hr(a = akde[[1]], par='high', l.ud=0.95),
-#' # find diffusion estimates
-#' diffusion = map_dbl(model, \(.m) {
-#'   if(any(grepl('diffusion',
-#'                rownames(summary(.m)$CI)))) {
-#'     return(summary(.m, units = FALSE)$
-#'              CI['diffusion (square meters/second)', 'est'])
-#'   } else {
-#'     return(NA_real_)
-#'   }}),
-#' # find speed
-#' speed = map_dbl(model, \(.m) {
-#'   if(any(grepl('speed',
-#'                rownames(summary(.m)$CI)))) {
-#'     return(summary(.m, units = FALSE)$
-#'              CI['speed (meters/second)', 'est'])
-#'   } else {
-#'     return(NA_real_)
-#'   }}),
-#' # find degrees of freedom
-#' dof_area = summary(model[[1]])$DOF['area'],
-#' dof_diff = if_else(
-#'   condition = grepl('OU', summary(model[[1]])$name),
-#'   true = summary(model[[1]])$DOF['speed'],
-#'   false = NA_real_),
-#' dof_speed = if_else(
-#'   condition = grepl('OUF', toupper(summary(model[[1]])$name)),
-#'   true = summary(model[[1]])$DOF['speed'],
-#'   false = NA_real_))
-#' })) %>% # close function for imap()
-#'   tidyr::unnest(models) %>%
-#'   #' `ctmm::%#%` syntax: `"new units" %#% value in SI units`
-#'   mutate(units_area = 'km^2',
-#'          area = units_diff %#% area,
-#'          units_diff = 'km^2/day',
-#'          diffusion = units_diff %#% diffusion,
-#'          units_speed = 'km/day',
-#'          speed = units_speed %#% speed,
-#'          t_center = (t_start + t_end) / 2,
-#'          posixct = as.POSIXct(t_center, origin = '1970-01-01',
-#'                               tz = .tel@info$timezone),
-#'          date = as.Date(posixct))
+# save movement models ----
+saveRDS(d, 'models/full-telemetry-movement-models.rds')
