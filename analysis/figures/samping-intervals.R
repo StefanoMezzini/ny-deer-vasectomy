dba11e41723f5f23b4323a17288392bda45303a7
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-08-06 12:31:33 -0700

    added panel of sampling interval

--- analysis/figures/samping-intervals.R
+++ analysis/figures/samping-intervals.R
@@ -3,6 +3,7 @@ library('tidyr')     # for data wrangling
 library('purrr')     # for functional programming
 library('lubridate') # for working with dates
 library('ggplot2')   # for fancy plots
+library('ctmm')      #' for movement modeling and `%#%`
 source('analysis/figures/default-theme.R')
 
 d <- readRDS('models/full-telemetry-movement-models-2024-04-20.rds') %>%
@@ -13,25 +14,50 @@ d <- readRDS('models/full-telemetry-movement-models-2024-04-20.rds') %>%
             study_year = paste('Year', study.year),
             tel = map(tel, data.frame)) %>%
   unnest(tel) %>%
+  # create a column of days since August 1st
+  mutate(date = as.Date(timestamp),
+         days_since_aug_1 =
+           if_else(
+             # if in Aug, Sept, Oct, Nov, or Dec
+             month(date) >= 8,
+             # then calculate days since Aug 1
+             date - as.Date(paste0(year(date), '-08-01')),
+             # otherwise calculate days since Aug 1 of previous year 
+             date - as.Date(paste0(year(date) - 1, '-08-01'))) %>%
+           as.numeric()) # convert difftime to numeric
+d
+
+d_dt <- d %>%
   group_by(animal, Sex, site, study_year) %>%
   summarize(dt = median(diff(timestamp), na.rm = TRUE)) %>%
   ungroup() %>%
   mutate(dt = as.numeric(dt) / (1 %#% 'hour'))
-d
 
-ggplot(d, aes(animal, dt)) +
-  facet_grid(study_year ~ .) +
-  geom_text(aes(label = animal), filter(d, dt > 2),
-            nudge_y = -0.2, nudge_x = 0.5) +
-  geom_vline(aes(xintercept = animal),
-             filter(d, duplicated(animal)) %>%
-               select(! study_year), alpha = 0.1) +
-  geom_point(aes(shape = Sex, color = site)) +
-  labs(x = NULL, y = 'Median sampling interval (hours)') +
-  scale_color_brewer('Site', type = 'qual', palette = 1) +
-  theme(axis.text.x.bottom = element_text(angle = 90, face = 'plain',
-                                          size = 7),
-        legend.position = 'top')
+# make figure of doy and days since August 1st
+cowplot::plot_grid(
+  ggplot(d_dt, aes(animal, dt)) +
+    facet_grid(study_year ~ .) +
+    geom_vline(aes(xintercept = animal),
+               filter(d, duplicated(animal)) %>%
+                 select(! study_year), alpha = 0.1) +
+    geom_point(aes(shape = Sex, color = site)) +
+    geom_text(aes(label = animal), filter(d_dt, dt > 2),
+              nudge_y = -0.2, nudge_x = 0.5) +
+    labs(x = NULL, y = 'Median sampling interval (hours)') +
+    scale_color_brewer('Site', type = 'qual', palette = 1) +
+    theme(axis.text.x.bottom = element_text(angle = 90, face = 'plain',
+                                            size = 7),
+          legend.position = 'top'),
+  ggplot() +
+    geom_histogram(aes(yday(date)), d, fill = 'grey', color = 'black',
+                   center = 5, binwidth = 10) +
+    labs(x = 'Day of year', y = 'Count'),
+  ggplot() +
+    geom_histogram(aes(days_since_aug_1), d, fill = 'grey', color = 'black',
+                   center = 5, binwidth = 10) +
+    labs(x = expression(bold(Days~since~August~1^bold(st))), y = 'Count') +
+    xlim(c(0, 370)),
+  ncol = 1, labels = 'AUTO', rel_heights = c(1.5, 1, 1))
 
-ggsave('figures/sampling-intervals.png', width = 12, height = 4,
-       units = 'in', dpi = 600, bg = 'white')
+ggsave('figures/dt-and-yday-days-since-august-first-hist.png',
+       width = 12, height = 8, dpi = 600, bg = 'white')
