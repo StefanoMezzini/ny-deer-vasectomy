cca74c48876ed1a47218f0cfd3befd9566243648
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-08-19 19:12:46 -0700

    increased nbins to make states more distiguishable

--- analysis/figures/accelerometry-histograms-states.R
+++ analysis/figures/accelerometry-histograms-states.R
@@ -29,24 +29,25 @@ d <-
                              'low activity',
                              'medium activity',
                              'high activity')),
-         log_VeDBA = if_else(VeDBA == 0, log(min(d$VeDBA[which(d$VeDBA > 0)])) - 3, log_VeDBA))
-
-d <- d %>%
-  mutate(month = factor(month, levels = unique(month)))
+         # place -Inf log(VeDBA) to min - 3 rather than - Inf
+         log_VeDBA = if_else(VeDBA == 0,
+                             log(min(VeDBA[which(VeDBA > 0)])) - 3,
+                             log_VeDBA),
+         month = factor(month, levels = unique(month)))
 
 min_log_VeDBA <- log(min(d$VeDBA[which(d$VeDBA > 0)]))
 
 p <-
   d %>%
   ggplot(aes(log_VeDBA, fill = State)) +
-  geom_histogram(color = 'black', bins = 50) +
+  geom_histogram(bins = 300) +
   geom_vline(xintercept = 1.31, lty = 'dashed') +
   geom_vline(xintercept = 4.17, lty = 'dashed') +
-  
   labs(x = expression(bold(log(VedBA))), y = 'Count (square-root scale)') +
   scale_fill_manual(values = pal) +
   scale_y_continuous(transform = 'sqrt') +
-  theme(legend.position = 'top')
+  theme(legend.position = 'top'); p
+
 ggsave('figures/vedba-histogram.png',
        plot = p, width = 6, height = 4, units = 'in', dpi = 600,
        bg = 'white')
