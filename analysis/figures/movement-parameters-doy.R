ab674a4dd0c7491fb53f67526d89f431f282d3b6
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-08-20 11:01:37 -0700

    finalized figure without faceting

--- analysis/figures/movement-parameters-doy.R
+++ analysis/figures/movement-parameters-doy.R
@@ -74,68 +74,44 @@ plot_grid(get_plot_component(p_hr + theme(legend.position = 'top'),
           plot_grid(p_hr, p_speed, p_diff, p_exc, labels = 'AUTO',
                     ncol = 2),
           ncol = 1, rel_heights = c(1, 10))
-ggsave('figures/mean-movement-parameters-doy.png',
+ggsave('figures/mean-movement-parameters-doy-faceted.png',
        width = 16, height = 12 * 1.1, dpi = 600, bg = 'white')
 
 # single figure for all parameters with no faceting for ease of comparison ----
+make_plot <- function(filename) {
+  readRDS(filename) %>%
+    mutate(
+    sex = factor(sex, levels = c('Female', 'Male'))) %>%
+    ggplot() +
+    geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
+    geom_ribbon(aes(date, ymin = lwr_95, ymax = upr_95, fill = site,
+                    group = sex_treatment), alpha = 0.3) +
+    geom_line(aes(date, mu, color = site, lty = sex), lwd = 1) +
+    scale_color_brewer('Site', type = 'qual', palette = 1,
+                       aesthetics = c('color', 'fill')) +
+    theme(legend.position = 'top') +
+    scale_x_continuous(NULL, breaks = DATES, labels = LABS,
+                       limits = as.Date(c('2021-10-01', '2022-04-30'))) +
+    scale_linetype_manual('Sex', values = c(2, 1)) +
+    theme(legend.position = 'none')
+}
+
 plot_grid(
   get_plot_component(
-    readRDS('models/predictions/hr-preds_mu.rds') %>%
-      ggplot() +
-      geom_ribbon(aes(date, ymin = lwr_95, ymax = upr_95, fill = sex_treatment),
-                  alpha = 0.2) +
-      geom_line(aes(date, mu, color = sex_treatment, lty = sex), lwd = 1) +
-      scale_color_brewer('sex_treatment', type = 'qual', palette = 2,
-                         aesthetics = c('color', 'fill')) +
+    make_plot('models/predictions/hr-preds_mu.rds') +
       theme(legend.position = 'top'),
     pattern = 'guide-box-top', return_all = TRUE),
   plot_grid(
-    readRDS('models/predictions/hr-preds_mu.rds') %>%
-      ggplot() +
-      geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
-      geom_ribbon(aes(date, ymin = lwr_95, ymax = upr_95, fill = sex_treatment),
-                  alpha = 0.2) +
-      geom_line(aes(date, mu, color = sex_treatment, lty = sex), lwd = 1) +
-      scale_color_brewer('sex_treatment', type = 'qual', palette = 2,
-                         aesthetics = c('color', 'fill')) +
-      scale_x_continuous(NULL, breaks = DATES, labels = LABS,
-                         limits = as.Date(c('2021-10-01', '2022-04-30'))) +
-      ylab(expression(bold('Mean 7-day HR size'~(km^2)))) +
-      theme(legend.position = 'none'),
-    readRDS('models/predictions/speed-preds_mu.rds') %>%
-      ggplot() +
-      geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
-      geom_ribbon(aes(date, ymin = lwr_95, ymax = upr_95, fill = sex_treatment),
-                  alpha = 0.2) +
-      geom_line(aes(date, mu, color = sex_treatment, lty = sex), lwd = 1) +
-      scale_color_brewer('sex_treatment', type = 'qual', palette = 2,
-                         aesthetics = c('color', 'fill')) +
-      scale_x_continuous(NULL, breaks = DATES, labels = LABS,
-                         limits = as.Date(c('2021-10-01', '2022-04-30'))) +
-      ylab('Mean distance travelled (km/day)') +
-      theme(legend.position = 'none'),
-    readRDS('models/predictions/diffusion-preds_mu.rds') %>%
-      ggplot() +
-      geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
-      geom_ribbon(aes(date, ymin = lwr_95, ymax = upr_95, fill = sex_treatment),
-                  alpha = 0.2) +
-      geom_line(aes(date, mu, color = sex_treatment, lty = sex), lwd = 1) +
-      scale_color_brewer('sex_treatment', type = 'qual', palette = 2,
-                         aesthetics = c('color', 'fill')) +
-      scale_x_continuous(NULL, breaks = DATES, labels = LABS,
-                         limits = as.Date(c('2021-10-01', '2022-04-30'))) +
-      ylab(expression(bold('Mean diffusion'~(km^2/day)))) +
-      theme(legend.position = 'none'),
-    readRDS('models/predictions/density-preds_mu.rds') %>%
-      ggplot() +
-      geom_vline(xintercept = REF_DATES[c(1, 3)], col = 'red') +
-      geom_ribbon(aes(date, ymin = lwr_95, ymax = upr_95, fill = sex_treatment),
-                  alpha = 0.2) +
-      geom_line(aes(date, mu, color = sex_treatment, lty = sex), lwd = 1) +
-      scale_color_brewer('sex_treatment', type = 'qual', palette = 2,
-                         aesthetics = c('color', 'fill')) +
-      scale_x_continuous(NULL, breaks = DATES, labels = LABS,
-                         limits = as.Date(c('2021-10-01', '2022-04-30'))) +
-      ylab('Mean daily excursivity') +
-      theme(legend.position = 'none')),
+    make_plot('models/predictions/hr-preds_mu.rds') +
+      ylab(expression(bold('Mean 7-day HR size'~(km^2)))),
+    make_plot('models/predictions/speed-preds_mu.rds') +
+      ylab('Mean distance travelled (km/day)'),
+    make_plot('models/predictions/diffusion-preds_mu.rds') +
+      ylab(expression(bold('Mean diffusion'~(km^2/day)))),
+    make_plot('models/predictions/density-preds_mu.rds') +
+      ylab('Mean daily excursivity'),
+    labels = 'AUTO'),
   ncol = 1, rel_heights = c(1, 10))
+
+ggsave('figures/mean-movement-parameters-doy.png',
+       width = 16, height = 8 * 1.1, dpi = 600, bg = 'white')
