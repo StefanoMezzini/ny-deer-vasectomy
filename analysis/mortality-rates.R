946891a99f229e31f885076f97994ae71e686729
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2025-02-21 10:46:00 -0800

    added code to calculate mortality rates

---
+++ analysis/mortality-rates.R
@@ -0,0 +1,13 @@
+library('dplyr')
+
+# rates for both years at once
+tibble(sex = c('all', 'male', 'female', 'all', 'male', 'female'),
+       group = c('treatment', 'treatment', 'treatment',
+                 'control', 'control', 'control'),
+       deaths = c(5, 3, 2, 8, 2, 6),
+       n = c(64, 21, 43, 60, 19, 41), # collared twice appear twice
+       perc = deaths / n * 100, # percent deaths
+       perc_se = sqrt(perc * (100 - perc) / n)) %>%
+  mutate(perc = round(perc, 1),
+         perc_se = round(perc_se, 1)) %>%
+  as.data.frame() # to avoid rounding
